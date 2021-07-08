f63dec05e605a91e3650ca04946d536ff5b672fe
caiofov <cfoviana@gmail.com>
2021-07-08 11:48:57 -0300

    aula 17

--- functions.php
+++ functions.php
@@ -1,24 +1,40 @@
 <?php /* Não precisa fechar a tag do php nesse arquivo */
 
 function load_scripts(){/* carrega as folhas de estilo utilizando uma função do wordpress que enfileira os arquivos */
+    /*Adicionando o bootstrap */
+    wp_enqueue_script('bootstrap-js', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.jss', array('jquery'), '4.0.0', true);
+    /*1- identificador 2-caminho 3-dependencia (jquery já vem no wordpress) 4-versao 5-indica se quer inserir no footer ou na head (padrão é false). Colocaremos antes o fechamento do body*/
+    wp_enqueue_style( 'bootstrap-css', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', array(), '4.0.0','all');
+    
+    
+    /*Adicionando a folha de estilo */
     wp_enqueue_style( 'template', get_template_directory_uri().'/css/template.css', array(), '1.0','all');
     
     /*Parâmetros da função pronta: 
     
-    1- identificador: Pode ser qualquer nome, de preferência o nome do arquivo, desde que não repita, pois precisam ser únicos
-    
-    2- caminho: diretório até o arquivo. Utilizaremos uma função do wordpress (get_template_directory()) que imprime o diretório até o arquivo atual. Dessa forma, concatentaremos apenas o diretório relativo até o arquivo da folha de estilo
-    
-    3- Dependência: informa se o arquivo a ser carregado depende de outros arquivos, isto é, se outros arquivos precisam ser carregados antes dele. Deve ser passado como um array o nome de cada um. Nesse caso, passaremos um array vazio, pois não precisamos de nenhum.
-
-    4- Versão
-
-    5- Tipo de mídia: qual tipo de arquivo o script se refere. Pode ser 'print', 'screen' ou 'all'. Esse último é o mais utilizado.
+    1- IDENTIFICADOR ('id' do html): Pode ser qualquer nome, de preferência o nome do arquivo, desde que não repita, pois precisam ser únicos
+    2- CAMINHO: diretório até o arquivo. Utilizaremos uma função do wordpress (get_template_directory()) que imprime o diretório até o arquivo atual. Dessa forma, concatentaremos apenas o diretório relativo até o arquivo da folha de estilo
+    3- DEPENDÊNCIA: informa se o arquivo a ser carregado depende de outros arquivos, isto é, se outros arquivos precisam ser carregados antes dele. Deve ser passado como um array o nome de cada um. Nesse caso, passaremos um array vazio, pois não precisamos de nenhum.
+    4- VERSÃO
+    5- TIPO DE MÍDIA: qual tipo de arquivo o script se refere. Pode ser 'print', 'screen' ou 'all'. Esse último é o mais utilizado.
     */
 
+    
 
 }
 
 /* Mas nenhuma função é executada "do nada" no wordpress. Precisamos relacioná-la a uma ação, ou seja, um "gancho" (hook), que será executada quando o wordpress for. Com outras palavras, é uma maneira de ligar nossas funções ao wordpress e, assim, executá-las*/
 add_action('wp_enqueue_scripts', 'load_scripts');
-/*1- Gancho / 2- função a ser executada */
+/*1- Gancho | 2- função a ser executada */
+
+
+//Registrando menus
+register_nav_menus(
+    array(
+        'my_main_menu' =>'Main Menu'
+    )
+);
+/*Recebe um array onde são encontrados todos os menus a serem registrados (podem ser registrados mais de um por vez) 
+1- Slug do menu: nome curto de identificação. É chamado de chave no array.
+Depois da => é o nome que o menu terá dentro da área de administração do wordpress.
+*/
--- header.php
+++ header.php
@@ -2,23 +2,41 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width">
     <title>Curso Wordpress</title>
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
-    <p>Este é um parágrafo</p>
     <header>
         
         <section class="top-bar">
-            <div class="social-media-icons">Icones Sociais</div>
-            <div class="search">Pesquisa</div>
+            <div class="container">
+                <div class="row">
+                    
+                    <div class="social-media-icons col-xl-9 col-sm-7 col-6">Icones Sociais</div>
+                    <div class="search col-xl-3 col-sm-5 col-6 text-right">Pesquisa</div>
+                
+                </div>
+            </div>
+    
         </section>
         
         <section class="menu-area">
-            <div class="logo">Logo</div>
-            <nav class="menu">Menu</nav>
+            <div class="container">
+                <div class="row">
+                    <div class="logo col-md-2 col-12 text-center">Logo</div>
+                    <nav class="menu col-md-10 text-right"><?php wp_nav_menu(array(
+                        'theme_location' => 'my_main_menu'
+                        ));?></nav> 
+                    <!--
+                    Função para exibir o menu dentro do tema. Ela recebe um array de configurações do menu.
+                    1- Theme Location: slug do menu
+                    Podem ser definidas muitas outras configurações
+                    -->
+                </div>
+            </div>
+            
         </section>
     
     </header>
