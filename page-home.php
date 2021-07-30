ab47337f84de39cb6a562a0e8813dd6c5e4d7f64
caiofov <cfoviana@gmail.com>
2021-07-29 23:25:23 -0300

    28: our services widget area

--- page-home.php
+++ page-home.php
@@ -12,8 +12,35 @@
 
             <section class="services">
                 <div class="container">
+                    <h1> Our Services</h1>
                     <div class="row">
-                        Serviços
+                        <div class="col-sm-4">
+                            <div class="service-item">
+                                <?php
+                                if(is_active_sidebar('services-1')){
+                                    dynamic_sidebar('services-1');}
+                                
+                                ?>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="service-item">
+                            <?php
+                                if(is_active_sidebar('services-2')){
+                                    dynamic_sidebar('services-2');
+                                }
+                                ?>
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <div class="service-item">
+                            <?php
+                                if(is_active_sidebar('services-3'))
+                                    {dynamic_sidebar('services-3');}
+                                
+                                ?>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
